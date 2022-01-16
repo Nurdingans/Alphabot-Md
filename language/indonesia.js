bcd565d4a6088791d5764adf9306f2dd34820398
Nurdingans <96080159+Nurdingans@users.noreply.github.com>
2022-01-16 08:35:06 +0700

    Update indonesia.js

--- language/indonesia.js
+++ language/indonesia.js
@@ -87,7 +87,7 @@ exports.notNum = (q) => {
 }
 exports.menunya = (salam, pushname) =>{
 	return`Hai kak ${pushname} 👋 selamat ${salam} , 
-saya Alphabot-Md, bot ini adalah Beta Multi-Device Whatsapp. 
+saya TzydinBotz, bot ini adalah Beta Multi-Device Whatsapp. 
 Jika kamu menemukan semacam bug atau kesalahan mohon dimaklumi dulu ya, 
 lapor owner agar segera di perbaiki 🙏      
 `
@@ -95,7 +95,7 @@ lapor owner agar segera di perbaiki 🙏
 exports.listMenu = (time, salam, pushname, prefix) => {
     return `*Selamat ${salam} ${pushname} 😖*
 *⌚Time Server : ${time}*
-*📚 List-Menu Alphabot-Md :*
+*📚 List-Menu TzydinBotz :*
 
 ╭─❒ 「 Bot Info 」 
 ├ ${prefix}owner
